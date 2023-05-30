85e561608c13d75e3e68fb41529dacdaf16142bd
AndSte01 <steger.andreas.2707@gmail.com>
2023-05-30 15:47:39 +0200

    implemented draft of remove moved to laravel internal validators

--- app/Http/Controllers/competitionController.php
+++ app/Http/Controllers/competitionController.php
@@ -8,7 +8,9 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Exceptions\MultiException;
 use Carbon\Carbon;
-use Carbon\Exceptions\InvalidFormatException;
+use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 
 class competitionController extends Controller
 {
@@ -80,56 +82,92 @@ class competitionController extends Controller
 
         // check for errors in provided competitions
         foreach ($content as $input_competition) {
-            // we need to convert the date from timestamp to Carbon Object (only if it exists)
-            if (!empty($input_competition->date)) {
-                try {
-                    // try to generate Carbon object form timestamp (in case an exception is thrown catch it, add it to the list and continue)
-                    $input_competition->date = Carbon::parse($input_competition->date); // note types are changed
-                } catch (\Throwable $th) {
-                    $exceptions[] = $th;
-                    continue; // continue with next element in list
-                }
-            }
+            // cast the input (is an object) to an array
+            $input_competition = (array) $input_competition;
 
-            // converting the integers
-            if (!empty($input_competition->feature_set)) {
-                if (($input_competition->feature_set = filter_var($input_competition->feature_set, FILTER_VALIDATE_INT)) === false) { // note: typed comparison mandatory
-                    $exceptions[] = new InvalidFormatException("could not parse 'feature_set' to int.");
-                    continue;
-                }
-            }
-            if (!empty($input_competition->areas)) {
-                if (($input_competition->areas = filter_var($input_competition->areas, FILTER_VALIDATE_INT)) === false) {
-                    $exceptions[] = new InvalidFormatException("could not parse 'areas' to int.");
-                    continue;
-                }
-            }
-
-            // converting live to boolean (no exception handling since the method used above doesn't work and there hasn't been any interest in doing the required research to implement it correctly)
-            if (!empty($input_competition->live))
-                $input_competition->live = filter_var($input_competition->live, FILTER_VALIDATE_BOOLEAN);
-
-            // TODO a competition might also be identified by it's name and date (prevent duplicates)
-            // TODO add password protection
+            /** @var bool variable stores wether a new competition has been created or an existing one gets updated */
+            $new_competition = false;
 
             // if an id has been provided search for the model in the database
-            if (!empty($input_competition->id)) {
+            if (!empty($input_competition["id"])) {
                 try {
-                    $competition = competition::findOrFail($input_competition->id);
+                    $competition = competition::findOrFail($input_competition["id"]);
                 } catch (\Throwable $th) {
                     $exceptions[] = $th;
+                    continue;
+                }
+
+                // check wether the users match, else throw exception
+                if ($competition->checkAccess($request->user()->id)) {
+                    $exceptions[] = new AccessDeniedException("competition?id=" . strval($competition->id));
                     continue;
                 }
             } else {
+                // TODO add check for duplicates
+
                 $competition = new competition();
+                $competition->user_id = $request->user()->id;
+                $new_competition = true;
             }
 
-            // now we fill the competition with the parsed data (we can safely overwrite id since we checked wether such an competition exists or not)
-            $competition->fill((array) $input_competition);
+            // only mangle with dates if they get touched by the request
+            if (isset($input_competition["date_start"]) || isset($input_competition["date_end"])) {
+                /**
+                 * get the relevant date and store it in an variable
+                 * $temp_date_start is set to one of the following variables in the given priority, if the higher prioritized one is null the one following is used (and so on)
+                 * 'date provided by the request ("the new on")' > 'date already stored in the competition ("the old one")' > 'the other date (implicitly assuming a single day competition)'
+                 * 
+                 * Note: - The case in which neither $input_competition["date_start"] nor $input_competition["date_end"] is set/provided, is already taken int account by the enclosing
+                 *         if statement (the one, that is one level up from this code)
+                 *       - The provided date (if one is provided by the request) get's directly parsed to an Carbon object.
+                 *       - The try-catch block is required because the request might provide invalid timestamps that Carbon can't parse (throwing an exception)
+                 *       - The variable $input_competition["date_..."] gets reused for the newly determined date
+                 */
+                try {
+                    $input_competition["date_start"] = isset($input_competition["date_start"])
+                        ? Carbon::parse($input_competition["date_start"])
+                        : (!empty($competition->date_start)
+                            ? $competition->date_start
+                            : Carbon::parse($input_competition["date_end"]));
 
-            // add the competition to the list for saving (since nothing is written to the db if only one exception did occur, we can skip this step once we detected one)
-            if (empty($exceptions))
-                $competitions[] = $competition;
+                    $input_competition["date_end"] = isset($input_competition["date_end"])
+                        ? Carbon::parse($input_competition["date_end"])
+                        : (!empty($competition->date_end)
+                            ? $competition->date_end
+                            : $input_competition["date_start"]); // since one of the both fields must be set we can assume that $temp_date_start contains a date provided by the request
+                } catch (\Throwable $th) {
+                    $exceptions[] = $th;
+                    continue;
+                }
+
+                // check that temp_date_end is before temp_date_start, if not set the ending date to the starting on (single day competition)
+                if ($input_competition["date_end"]->timestamp < $input_competition["date_start"]->timestamp) {
+                    $input_competition["date_end"] = $input_competition["date_start"];
+                }
+            }
+
+            // validate remaining fields
+            $validator = Validator::make($input_competition, [
+                'feature_set' => 'nullable|numeric',
+                'areas' => 'nullable|numeric|min:1',
+                'live' => 'nullable|boolean'
+            ]);
+
+            // do the validation in case it failed write exception and continue
+            if ($validator->fails()) {
+                $exceptions[] = new InvalidArgumentException($validator->errors()->toJson());
+                continue;
+            }
+
+            // should be unnecessary in case of correct implementation
+            // if (empty($exceptions))
+            //     continue;
+
+            // fill in the data (that has been validated to be correct) into the competition
+            $competition->fill($input_competition);
+
+            // add the competition to the list
+            $competitions[] = $competition;
         }
 
         // check wether exceptions did occur if so throw an exception
@@ -139,6 +177,7 @@ class competitionController extends Controller
         // no exceptions did occur we now write the competitions to the database
         foreach ($competitions as $competition) {
             $competition->save();
+            $competition->refresh(); // we want to provide an exact copy whats in the db so we re-hydrate the model with exactly the databases content
         }
 
         // return the save competitions
@@ -148,11 +187,27 @@ class competitionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param int  $id
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy($id): JsonResponse
+    public function destroy(Request $request): JsonResponse
     {
-        //
+        // TODO add support for implicit identification
+
+        $id = abs(intval($request->input('id')));
+        if ($id == null)
+            throw new InvalidArgumentException("Parameter 'id' hasn't been provided");
+
+        // try to find competition in the database
+        $competition = competition::findOrFail($id);
+
+        // check wether the current user has access
+        if (!$competition->checkAccess($request->user()->id))
+            throw new AccessDeniedException("competition?id=" . strval($competition->id));
+
+        // delete record in the database
+        $competition->delete();
+
+        return new JsonResponse(["SUCCESS"]);
     }
 }
