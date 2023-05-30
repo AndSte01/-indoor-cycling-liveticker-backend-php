2f4e2040aaa273e7b9aa6739596c1980a4e4ecee
AndSte01 <steger.andreas.2707@gmail.com>
2023-05-30 17:06:53 +0200

    moved again

--- app/Models/Traits_Interfaces/ImplicitIdentificationInterface.php
+++ app/Models/Traits_Interfaces/ImplicitIdentificationInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Traits_Interfaces;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
--- app/Models/Traits_Interfaces/ImplicitIdentificationTrait.php
+++ app/Models/Traits_Interfaces/ImplicitIdentificationTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Traits_Interfaces;
 
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
